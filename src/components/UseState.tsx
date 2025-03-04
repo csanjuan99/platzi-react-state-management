99c7f3fa23464771cd6548c02cb65acc7ae8e2ea
Carlos Sanjuan <hello@carlossanjuan.co>
2025-03-04 08:52:16 -0500

    Efectos con useEffect

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -7,6 +7,19 @@ interface Props {
 const UseState = ({name}: Props) => {
 
     const [error, setError] = React.useState<boolean>(false);
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        console.log('Empezando efecto...')
+        if (isLoading) {
+            setTimeout(() => {
+                console.log('Cambiando estado...')
+                setIsLoading(false);
+                console.log('Estado cambiado con éxito...')
+            }, 1000);
+        }
+        console.log('Terminando efecto...')
+    }, [isLoading]);
 
     return (
         <>
@@ -17,11 +30,13 @@ const UseState = ({name}: Props) => {
                 Por favor, ingresa el código de seguridad
             </p>
 
+            {isLoading && <p>Cargando...</p>}
+
             {error && <p style={{color: 'red'}}>Código incorrecto</p>}
 
             <input type="text" placeholder="Codigo de seguridad"/>
             <button
-                onClick={() => setError(!error)}
+                onClick={() => setIsLoading(true)}
             >
                 Comprobar
             </button>
